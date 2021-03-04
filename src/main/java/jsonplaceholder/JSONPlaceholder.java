8274fb3e5b61609f1de7e38db3f3e7d3566dfe1e
Kandziakou <kandziakou@gmail.com>
2021-03-04 10:19:31 +0300

    code style was fixed

--- src/main/java/jsonplaceholder/JSONPlaceholder.java
+++ src/main/java/jsonplaceholder/JSONPlaceholder.java
@@ -4,16 +4,10 @@ import lombok.Getter;
 
 public class JSONPlaceholder {
     private final String baseUrl = "http://jsonplaceholder.typicode.com";
-    @Getter private final String
-            posts = baseUrl + "/posts";
-    @Getter private final String
-            comments = baseUrl + "/comments";
-    @Getter private final String
-            users = baseUrl + "/users";
-    @Getter private final String
-            post = posts +"/%d";
-    @Getter private final String
-            comment = comments + "/%d";
-    @Getter private final String
-            user = users + "/%d";
+    @Getter private final String posts = baseUrl + "/posts";
+    @Getter private final String comments = baseUrl + "/comments";
+    @Getter private final String users = baseUrl + "/users";
+    @Getter private final String post = posts +"/%d";
+    @Getter private final String comment = comments + "/%d";
+    @Getter private final String user = users + "/%d";
 }
