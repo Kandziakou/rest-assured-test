14b1ca645c2650903dc517e4a85839d92a3d3d57
Kandziakou <kandziakou@gmail.com>
2021-03-04 12:54:16 +0300

    minor improvements

--- src/test/java/apichecking/CheckMethods.java
+++ src/test/java/apichecking/CheckMethods.java
@@ -11,87 +11,83 @@ import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
-public class CheckMethods extends JSONPlaceholder{
+public class CheckMethods {
+    private final JSONPlaceholder jsonPlaceholder = new JSONPlaceholder();
 
-    private final String posts = getPosts();
-    private final String comments = getComments();
-    private final String users = getUsers();
-
-    private void get200(String route){
-        given()
-                .when().get(route)
-                .then().statusCode(200);
-    }
-
-    private void get404(String route){
-        given()
-                .when().get(route)
-                .then().statusCode(404);
-    }
+    private final String posts = jsonPlaceholder.getPosts();
+    private final String comments = jsonPlaceholder.getComments();
+    private final String users = jsonPlaceholder.getUsers();
+    private final String URI = jsonPlaceholder.getBaseURI();
+    private final String post = jsonPlaceholder.getPost();
+    private final String comment = jsonPlaceholder.getComment();
+    private final String user = jsonPlaceholder.getUser();
 
     private Response getJSON(String route){
         return given()
                 .when().get(route)
-                .then().contentType(ContentType.JSON).extract().response();
+                .then().statusCode(200).contentType(ContentType.JSON).extract().response();
+    }
+
+    private void get404(String path){
+        given()
+                .when().get(URI + path)
+                .then().statusCode(404);
     }
 
     @Test
     public void testGetAllPosts(){
         String body = "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto";
-        get200(posts);
         List<HashMap> jsonResponse = getJSON(posts).jsonPath().getList("$");
-        Assert.assertEquals(100, jsonResponse.size());
+        Assert.assertEquals(jsonPlaceholder.getPostsCount(), jsonResponse.size());
         Assert.assertEquals(jsonResponse.get(0).get("body"), body);
     }
 
     @Test
     public void testGetOnePost(){
-        int index = (int) (Math.random()*100);
-        String post = String.format(getPost(), index);
-        String notExistedPost = String.format(getPost(), index+100);
-        get200(post);
-        get404(notExistedPost);
-        int id = getJSON(post).jsonPath().get("id");
+        int index = (int) (Math.random() * jsonPlaceholder.getPostsCount());
+        String validPost = String.format(post, index);
+        int id = getJSON(URI + validPost).jsonPath().get("id");
         Assert.assertEquals(id, index);
+
+        String invalidPost = String.format(post, index + jsonPlaceholder.getPostsCount());
+        get404(invalidPost);
     }
 
     @Test
     public void testGetAllComments(){
         String email = "Eliseo@gardner.biz";
-        get200(comments);
         List<HashMap> jsonResponse = getJSON(comments).jsonPath().getList("$");
-        Assert.assertEquals(500, jsonResponse.size());
+        Assert.assertEquals(jsonPlaceholder.getCommentsCount(), jsonResponse.size());
         Assert.assertEquals(jsonResponse.get(0).get("email"), email);
     }
 
     @Test
     public void testGetOneComment(){
-        int index = (int) (Math.random()*500);
-        String comment = String.format(getComment(), index);
-        String notExistedComment = String.format(getComment(), index+100);
-        get200(comment);
-        get404(notExistedComment);
-        int id = getJSON(comment).jsonPath().get("id");
+        int index = (int) (Math.random()* jsonPlaceholder.getCommentsCount());
+        String validComment = String.format(comment, index);
+        int id = getJSON(validComment).jsonPath().get("id");
         Assert.assertEquals(id, index);
+
+        String invalidComment = String.format(comment, index + jsonPlaceholder.getCommentsCount());
+        get404(invalidComment);
     }
 
     @Test
     public void testGetAllUsers(){
         String email = "Sincere@april.biz";
-        get200(users);
         List<HashMap> jsonResponse = getJSON(users).jsonPath().getList("$");
-        Assert.assertEquals(10, jsonResponse.size());
+        Assert.assertEquals(jsonPlaceholder.getUsersCount(), jsonResponse.size());
         Assert.assertEquals(jsonResponse.get(0).get("email"), email);
     }
 
     @Test
     public void testGetOneUser(){
-        int index = (int) (Math.random()*10);
-        String user = String.format(getUser(), index);
-        String notExistedUser = String.format(getUser(), index+100);
-        get200(user);
-        get404(notExistedUser);
-        int id = getJSON(user).jsonPath().get("id");
+        int index = (int) (Math.random()* jsonPlaceholder.getUsersCount());
+        String validUser = String.format(user, index);
+        int id = getJSON(URI + validUser).jsonPath().get("id");
         Assert.assertEquals(id, index);
+
+        String invalidUser = String.format(user, index + jsonPlaceholder.getUsersCount());
+        get404(invalidUser);
     }
 }
